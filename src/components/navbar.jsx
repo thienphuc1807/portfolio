b858ecbcbd30334beb1ef1ae48761477ff188e28
thienphuc1807 <90585795+thienphuc1807@users.noreply.github.com>
2024-10-09 16:07:46 +0700

    Active Link Style for a Navbar Link

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -2,6 +2,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
+import NavLink from "./navLink";
 
 const links = [
     { url: "/", title: "Home" },
@@ -22,29 +23,27 @@ const social = [
 const Navbar = () => {
     const [open, setOpen] = useState(false);
     return (
-        <div className="h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-24 xl:px-48">
+        <div className="h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-24 xl:px-48 xl:text-xl">
             {/* Links */}
-            <div className="hidden md:flex gap-8 w-1/3">
+            <div className="hidden md:flex gap-4 lg:w-1/3">
                 {links.map((link) => (
-                    <Link href={link.url} key={link.title}>
-                        {link.title}
-                    </Link>
+                    <NavLink link={link} key={link.title} />
                 ))}
             </div>
             {/* Logo */}
-            <div className="md:hidden lg:flex lg:w-1/3 justify-center">
+            <div className="md:hidden lg:flex lg:justify-center lg:w-1/3">
                 <Link
                     href="/"
-                    className="bg-black rounded-md p-1 font-semibold flex items-center justify-center"
+                    className="text-sm bg-black rounded-md p-1 font-semibold flex items-center justify-center"
                 >
-                    <span className="text-white pr-1">PhucNguyen</span>
+                    <span className="text-white mr-1">PhucNguyen</span>
                     <span className="w-12 h-8 bg-white flex items-center justify-center">
                         .dev
                     </span>
                 </Link>
             </div>
             {/* Social */}
-            <div className="hidden md:flex gap-4 w-1/3">
+            <div className="hidden md:flex gap-4 lg:w-1/3 lg:justify-center">
                 {social.map((item) => (
                     <Link href={item.href} key={item.alt}>
                         <Image
